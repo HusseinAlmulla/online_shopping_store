34114ea64435d59f8eebdf64c061441b24155fc2
Ahmed <ahmed.info4.5@gmal.com>
2020-05-03 08:21:35 -0400

    creating landing page

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ function Footer() {
+    return (
+        <div>
+            <footer class="page-footer font-small blue pt-4">
+                <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                </div>
+
+            </footer>
+            
+        </div>
+    )
+}
+
+export default Footer;
