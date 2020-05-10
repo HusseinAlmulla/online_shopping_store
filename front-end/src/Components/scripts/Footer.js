8d9330e605a61034d303d184c555c29ec8052518
Hussein Almulla <hussenalmulla@gmail.com>
2020-05-10 13:14:18 -0400

    changed the copyright message

--- front-end/src/Components/scripts/Footer.js
+++ front-end/src/Components/scripts/Footer.js
@@ -5,7 +5,7 @@ import React from 'react'
         <div>
             <footer className="page-footer font-small blue pt-4">
                 <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                    <a href="https://mdbootstrap.com/"> WeLearn.com</a>
                 </div>
 
             </footer>
